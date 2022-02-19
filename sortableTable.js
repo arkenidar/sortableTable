1ef5c60049e4ee5f50301222ae722c493ebccda8
Dario Cangialosi Arkenidar (DCA) <dario.cangialosi@gmail.com>
2022-02-19 17:46:04 +0100

    comments for explanations, and 2 fixes (inplace "sort()" and :not(:first-of-type) )

--- sortableTable.js
+++ sortableTable.js
@@ -1,3 +1,4 @@
+// a table as an array of objects with the same structure (same attributes/fields)
 let table = [
 { name: 'Nightcrawler', realname: 'Wagner, Kurt',     power: 'Teleportation',    info: 'http://www.superherodb.com/Nightcrawler/10-107/' },
 { name: 'Cyclops',      realname: 'Summers, Scott',   power: 'Optic blast',      info: 'http://www.superherodb.com/Cyclops/10-50/' },
@@ -5,36 +6,64 @@ let table = [
 { name: 'Wolverine',    realname: 'Howlett, James',   power: 'Regeneration',     info: 'http://www.superherodb.com/Wolverine/10-161/' }
 ]
 
+// initally is sorted by "name"
 let sortColumn = "name"
+// changing "sortColumn" must activate the column action
 columnAction(sortColumn)
+
+// set "click event" handlers for every "sortable"
 for(let column of document.querySelectorAll("[data-sortable]")){
+    // column "on click" event handler function
     column.onclick = function(event){
+        // clicked DOM element
         let element = event.target
+        // get "column name"
         let columnName = element.dataset.sortable
+        // do column action specific for column name
         columnAction(columnName)
     }
 }
+
+// action when a table column is activated
 function columnAction(columnName){
+    
+    // no CSS class (part of CSS class change)
     setSortedClassName("",sortColumn)
     
+    // current column used for sorting
     sortColumn=columnName
-    table=table.sort((a, b) => a[sortColumn] > b[sortColumn] ? 1 : -1)
+    // sort by column
+    table.sort((a, b) => a[sortColumn] > b[sortColumn] ? 1 : -1)
+    // changing table means producing HTML again
     fillTable()
     
+    // set "sorted" CSS class (CSS class change)
     setSortedClassName("sorted",sortColumn)
+    
+    // CSS class for "sortable" (column name for selecting it)
     function setSortedClassName(className,columnName){
+        // "sortable" element attribute for querying
         let element = document.querySelector(`[data-sortable="${columnName}"]`)
+        // set CSS class of HTML DOM element
         element.className = className
     }
 
 }
 
+// produce the first table (initialization)
 fillTable()
+
+// produce HTML for <table> tag "body" using "table" array of objects
 function fillTable(){
+    // table "body"
     let tbody = document.querySelector("table#heroes tbody")
+    // empty it
     tbody.innerHTML = ""
+    // fill it again table row by table row
     for(const [index, entry] of table.entries()){
+        // the table entry has data for filling below
         let {info,name,realname,power} = entry
+        // table row (filled)
         tbody.innerHTML += `<tr>
             <td>${index+1}</td>
             <td><a href='${info}'>${name}</a></td>
