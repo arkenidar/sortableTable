d8cb9cd7f75a506c15a4a31fb12c6043c321b1dd
Dario Cangialosi Arkenidar (DCA) <dario.cangialosi@gmail.com>
2022-02-21 21:08:14 +0100

    separate files for "concrete table" definitions (separated from "abstract table" definition)

---
+++ tables/products.js
@@ -0,0 +1,30 @@
+// a table as an array of objects with the same structure (same attributes/fields)
+// table of products to sell (sample)
+let productsTableData = [
+    { name: 'Cookies',  cost: 9,     avail: '14'},
+    { name: 'Bread',    cost: 10,     avail: '21'},
+    { name: 'Water',    cost: 5,     avail: '28'},
+    { name: 'Juice',    cost: 6,     avail: '16'},
+]
+
+// product table object
+createTableObject(productsTableData.slice(),productsFillTable,"name","#products")
+
+// produce HTML for <table> tag "body" using "table" array of objects
+function productsFillTable(tableData,tableNode){
+    // table "body"
+    let tbody = tableNode.querySelector("tbody")
+    // empty it
+    tbody.innerHTML = ""
+    // fill it again table row by table row
+    for(const [index, entry] of tableData.entries()){
+        // the table entry has data for filling below
+        let {name,cost,avail} = entry
+        // table row (filled)
+        tbody.innerHTML += `<tr>
+            <td><a href="javascript:alert('${name}')">${name}</a></td>
+            <td>${cost}</td>
+            <td>${avail}</td>
+            </tr>`
+    }
+}
---
+++ tables/xmen.js
@@ -0,0 +1,33 @@
+// a table as an array of objects with the same structure (same attributes/fields)
+let tableData1 = [
+{ name: 'Nightcrawler', realname: 'Wagner, Kurt',     power: 'Teleportation',    info: 'http://www.superherodb.com/Nightcrawler/10-107/' },
+{ name: 'Cyclops',      realname: 'Summers, Scott',   power: 'Optic blast',      info: 'http://www.superherodb.com/Cyclops/10-50/' },
+{ name: 'Rogue',        realname: 'Marie, Anna',      power: 'Absorbing powers', info: 'http://www.superherodb.com/Rogue/10-831/' },
+{ name: 'Wolverine',    realname: 'Howlett, James',   power: 'Regeneration',     info: 'http://www.superherodb.com/Wolverine/10-161/' }
+]
+
+// first table object
+createTableObject(tableData1.slice(),fillTableCommon1,"name","#heroes")
+
+// second table object
+createTableObject(tableData1.slice(),fillTableCommon1,"name","#heroes2")
+
+// produce HTML for <table> tag "body" using "table" array of objects
+function fillTableCommon1(tableData,tableNode){
+    // table "body"
+    let tbody = tableNode.querySelector("tbody")
+    // empty it
+    tbody.innerHTML = ""
+    // fill it again table row by table row
+    for(const [index, entry] of tableData.entries()){
+        // the table entry has data for filling below
+        let {info,name,realname,power} = entry
+        // table row (filled)
+        tbody.innerHTML += `<tr>
+            <td>${index+1}</td>
+            <td><a href='${info}'>${name}</a></td>
+            <td>${realname}</td>
+            <td>${power}</td>
+            </tr>`
+    }
+}
